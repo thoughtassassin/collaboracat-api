e6644a405b61c9049293377fee5dac9618aa6dbd
David Krause <davidkrause@Davids-MBP.lan1>
2019-10-26 15:41:01 -0500

    Remove port from production command

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -32,10 +32,6 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     host: process.env.DB_HOST,
-    dialect: "postgres",
-    ssl: true,
-    dialectOptions: {
-      ssl: { require: true }
-    }
+    dialect: "postgres"
   }
 };
