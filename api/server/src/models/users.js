33660d21aee8245fdf1a8297d394d6df6bf8c9b2
David Krause <davidkrause@Davids-MBP.lan1>
2019-10-26 18:57:51 -0500

    Add authentication

--- api/server/src/models/users.js
+++ api/server/src/models/users.js
@@ -1,4 +1,6 @@
-"use strict";
+import bcrypt from "bcrypt";
+("use strict");
+
 module.exports = (sequelize, DataTypes) => {
   const Users = sequelize.define(
     "Users",
@@ -8,6 +10,24 @@ module.exports = (sequelize, DataTypes) => {
     },
     {}
   );
+  Users.beforeSave((user, options) => {
+    if (user.changed("password")) {
+      user.password = bcrypt.hashSync(
+        user.password,
+        bcrypt.genSaltSync(10),
+        null
+      );
+    }
+  });
+  Users.prototype.comparePassword = function(passw, cb) {
+    const password = this.password;
+    bcrypt.compare(passw, password, function(err, isMatch) {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, isMatch);
+    });
+  };
   Users.associate = function(models) {
     Users.belongsTo(models.Role);
     Users.belongsToMany(models.Feeds, {
