371f3134f0bfbb932479639546f7b595def31fbc
David Krause <davidkrause@Davids-MBP.lan1>
2019-10-27 18:21:23 -0500

    Protect Channel routes Protect Contact routes Protect Feed routes

--- api/server/routes/ContactRoutes.js
+++ api/server/routes/ContactRoutes.js
@@ -1,12 +1,34 @@
 import { Router } from "express";
 import ContactController from "../controllers/ContactController";
+import passport from "passport";
+import "../src/config/passport";
 
 const router = Router();
 
-router.get("/", ContactController.getAllContacts);
-router.post("/", ContactController.addContact);
-router.get("/:id", ContactController.getAContact);
-router.put("/:id", ContactController.updatedContact);
-router.delete("/:id", ContactController.deleteContact);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  ContactController.getAllContacts
+);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  ContactController.addContact
+);
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  ContactController.getAContact
+);
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  ContactController.updatedContact
+);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  ContactController.deleteContact
+);
 
 export default router;
--- api/server/routes/FeedRoutes.js
+++ api/server/routes/FeedRoutes.js
@@ -1,12 +1,34 @@
 import { Router } from "express";
 import FeedController from "../controllers/FeedController";
+import passport from "passport";
+import "../src/config/passport";
 
 const router = Router();
 
-router.get("/", FeedController.getAllFeeds);
-router.post("/", FeedController.addFeed);
-router.get("/:id", FeedController.getAFeed);
-router.put("/:id", FeedController.updatedFeed);
-router.delete("/:id", FeedController.deleteFeed);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  FeedController.getAllFeeds
+);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  FeedController.addFeed
+);
+router.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  FeedController.getAFeed
+);
+router.put(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  FeedController.updatedFeed
+);
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  FeedController.deleteFeed
+);
 
 export default router;
