2fb3990984c17bd08376f9206d0d78b4594fefb6
David Krause <davidkrause@Davids-MBP.lan1>
2019-11-02 18:56:11 -0500

    Add Role to Login token

--- api/server/services/LoginService.js
+++ api/server/services/LoginService.js
@@ -4,7 +4,16 @@ class LoginService {
   static async login(username) {
     try {
       return await database.Users.findOne({
-        where: { username: username }
+        where: { username: username },
+        include: [
+          {
+            model: database.Role,
+            as: "Role",
+            required: false,
+            // Pass in the Feed attributes that you want to retrieve
+            attributes: ["role"]
+          }
+        ]
       });
     } catch (error) {
       throw error;
